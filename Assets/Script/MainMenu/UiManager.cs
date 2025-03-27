da8fed710e41c9c79b70320895c09b5ed6776aab
Christincu <hs08793@gmail.com>
2025-03-27 21:57:19 +0900

    Add_Panel
    25.03.27

---
+++ Assets/Script/MainMenu/UiManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void offMainMenuUI()
+    {
+        
+        GameObject.FindWithTag("UI_MainMenu");
+        
+    }
+}
